66e3859b23174b91e38f5c49f27492a932325598
Veronica Sham <shamve@oocl.com>
2020-04-14 17:04:43 +0800

    [Veronica]-remove not necessary if conditions and variables in SmartParkingBoy

--- src/main/java/com/oocl/SmartParkingBoy.java
+++ src/main/java/com/oocl/SmartParkingBoy.java
@@ -4,26 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SmartParkingBoy extends ParkingBoy{
+public class SmartParkingBoy extends ParkingBoy {
 
-    private int countParkingLot;
 
     public SmartParkingBoy(ParkingLot... parkingLot) {
         super(parkingLot);
-        countParkingLot = super.getParkingLots().size();
     }
 
     public ParkingTicket parkCar(Car car) {
-        ParkingLot chooseParkingLot = super.getParkingLots().get(0);
+        ParkingLot chooseParkingLot;
         List<Integer> spaceLeftInParkingLot = new ArrayList<>();
-        if (countParkingLot > 1) {
-            for (ParkingLot parkingLot : super.getParkingLots()) {
-                spaceLeftInParkingLot.add(parkingLot.getSpaceLeftInParkingLot());
-            }
-            chooseParkingLot = super.getParkingLots().get(spaceLeftInParkingLot.indexOf(Collections.max(spaceLeftInParkingLot)));
+        for (ParkingLot parkingLot : super.getParkingLots()) {
+            spaceLeftInParkingLot.add(parkingLot.getSpaceLeftInParkingLot());
         }
+        chooseParkingLot = super.getParkingLots().get(spaceLeftInParkingLot.indexOf(Collections.max(spaceLeftInParkingLot)));
+
         return chooseParkingLot.parkCar(car);
     }
-
 
 }
