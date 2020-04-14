7f80e0ca163635b4261385831dfd44f4bc36fdbc
Veronica Sham <shamve@oocl.com>
2020-04-14 17:02:09 +0800

    [Veronica]- remove variables that are not used and not necessary if condition in SuperSmartParkingBoy

--- src/main/java/com/oocl/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/SuperSmartParkingBoy.java
@@ -5,24 +5,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
-    private int countParkingLot;
+
 
     public SuperSmartParkingBoy(ParkingLot... parkingLot) {
         super(parkingLot);
-        countParkingLot = super.getParkingLots().size();
+
     }
 
     public ParkingTicket parkCar(Car car) {
-        ParkingLot chooseParkingLot = super.getParkingLots().get(0);
+        ParkingLot chooseParkingLot;
         List<Float> availablePositionRate = new ArrayList<>();
-        if (countParkingLot > 1) {
-            for (ParkingLot parkingLot : super.getParkingLots()) {
-                availablePositionRate.add(parkingLot.getAvailablePositionRate());
-            }
-            chooseParkingLot = super.getParkingLots().get(availablePositionRate.indexOf(Collections.max(availablePositionRate)));
+
+        for (ParkingLot parkingLot : super.getParkingLots()) {
+            availablePositionRate.add(parkingLot.getAvailablePositionRate());
         }
+        chooseParkingLot = super.getParkingLots().get(availablePositionRate.indexOf(Collections.max(availablePositionRate)));
+
         return chooseParkingLot.parkCar(car);
     }
-
 
 }
